899e5944158b823028d14c00ba4a4f93ee7b3117
rishabh-malhotra <rishabhmalhotra110796@gmail.com>
2019-07-17 14:21:16 +0530

    added react-native-navigation and made conf changes in android folder for it to work

--- App.js
+++ App.js
@@ -1,94 +1,26 @@
-import React, { Component } from "react";
-import { StyleSheet, View } from "react-native";
-import PlaceInput from "./src/components/PlaceInput/PlaceInput";
-import PlaceList from "./src/components/PlaceList/PlaceList";
-//import placeImage from './src/assets/beautiful-place.jpg'
-import {addPlace,deletePlace,deselectPlace,selectPlace} from './src/store/actions';
-import {bindActionCreators} from "redux";
-import {connect} from "react-redux";
-import PlaceDetail from './src/components/PlaceDetail/PlaceDetail'
-class App extends Component {
-  placeAddedHandler = placeName => {
-    this.props.actions.addPlace(placeName);
-    // this.setState(prevState => {
-    //   return {
-    //     places: prevState.places.concat({
-    //       key: Math.random().toString(),
-    //       name: placeName,
-    //       image:{
-    //         uri:"https://www.planetware.com/photos-large/AUS/australia-beautiful-places-port-douglas.jpg"
-    //       }
-    //     })
-    //   };
-    // });
-  };
+import {Navigation} from 'react-native-navigation';
+import AuthScreen from './src/screens/Auth/Auth';
 
-  placeSelectedHandler = key => {
-    this.props.actions.selectPlace(key);
-    // this.setState(prevState=>{
-    //   return{
-    //     selectedPlace:prevState.places.find(place=>{
-    //       return place.key === key;
-    //     })
-    //   }
-    // });
-  };
+//Register screens
+Navigation.registerComponent("awesome-places.AuthScreen",()=>AuthScreen);
 
-  placeDeletedHandler=()=>{
-    this.props.actions.deletePlace();
-    // this.setState(prevState => {
-    //   return {
-    //     places: prevState.places.filter(place => {
-    //       return place.key !== prevState.selectedPlace.key;
-    //     }),
-    //     selectedPlace:null
-    //   };
-    // });
-  }
+//start the app
 
-  modalClosedHandler = () =>{
-    this.props.actions.deselectPlace();
-    // this.setState({
-    //   	selectedPlace:null
-    // })
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <PlaceDetail selectedPlace={/*this.state.selectedPlace*/this.props.selectedPlace}
-                      onItemDeleted={this.placeDeletedHandler}
-                      onModalClose={this.modalClosedHandler}
-        />
-        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
-        <PlaceList
-          places={/*this.state.places*/this.props.places}
-          onItemSelected={this.placeSelectedHandler}
-        />
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 26,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "flex-start"
+Navigation.setRoot({
+  root: {
+    stack: {
+      children: [{
+        component: {
+          name: 'awesome-places.AuthScreen',
+        }
+      }],
+      options: {
+        topBar: {
+          title: {
+            text: 'Login'
+          }
+        }
+      }
+    }
   }
 });
-
-const mapStateToProps = state => {
-  return {
-    places: state.places.places,
-    selectedPlace: state.places.selectedPlace
-  };
-};
-
-const mapDispatchToProps = (dispatch) => (
-   {actions:bindActionCreators({addPlace,deselectPlace,deletePlace,selectPlace},dispatch)}
-)
-
-export default connect(mapStateToProps,mapDispatchToProps)(App);
--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-import { AppRegistry } from 'react-native';
 import App from './App';
-import {Provider} from 'react-redux'
-import configureStore from './src/store/configureStore'
-import React from 'react'
-const store=configureStore();
-const AwesomePlacesApp =()=>(
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
 
-AppRegistry.registerComponent('AwesomePlaces', () => AwesomePlacesApp);
+// import { AppRegistry } from 'react-native';
+// import App from './App';
+// import {Provider} from 'react-redux'
+// import configureStore from './src/store/configureStore'
+// import React from 'react'
+// const store=configureStore();
+// const AwesomePlacesApp =()=>(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>
+// );
+
+// AppRegistry.registerComponent('AwesomePlaces', () => AwesomePlacesApp);
