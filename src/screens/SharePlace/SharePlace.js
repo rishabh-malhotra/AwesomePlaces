cd2977d794559f45cb33d91ff55be8ed11b3aa79
rishabh-malhotra <rishabhmalhotra110796@gmail.com>
2019-07-19 13:57:37 +0530

    added some more styling changes

--- src/screens/SharePlace/SharePlace.js
+++ src/screens/SharePlace/SharePlace.js
@@ -1,45 +1,106 @@
-import React,{Component} from 'react';
-import {View,Text} from 'react-native';
-import {connect} from 'react-redux'
-import {addPlace} from '../../store/actions'
-import {bindActionCreators} from "redux";
-import PlaceInput from '../../components/PlaceInput/PlaceInput'
-import {Navigation} from 'react-native-navigation'
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  Button,
+  TextInput,
+  StyleSheet,
+  ScrollView,
+} from "react-native";
+import { connect } from "react-redux";
+import { addPlace } from "../../store/actions";
+import { bindActionCreators } from "redux";
+import PlaceInput from "../../components/PlaceInput/PlaceInput";
+import { Navigation } from "react-native-navigation";
+import MainText from "../../components/UI/MainText/MainText";
+import HeadingText from "../../components/UI/HeadingText/HeadingText";
+import PickImage from "../../components/PickImage/PickImage";
+import PickLocation from "../../components/PickLocation/PickLocation";
 
-class SharePlace extends Component{
-    constructor(props){
-        super(props);
-        Navigation.events().bindComponent(this);
-        this.isSideDrawerVisible = false;
-      }
-      navigationButtonPressed({buttonId}) {
-        if (buttonId === "btn_toggle_drawer2") {
-          (!this.isSideDrawerVisible) ? this.isSideDrawerVisible = true : this.isSideDrawerVisible = false
-            Navigation.mergeOptions(this.props.componentId, {
-                sideMenu: {
-                    left: {
-                        visible:  this.isSideDrawerVisible
-                    }
-                }
-            });
+class SharePlace extends Component {
+  constructor(props) {
+    super(props);
+    Navigation.events().bindComponent(this);
+    this.isSideDrawerVisible = false;
+    this.state = {
+        placeName: ""
+      };
+  }
+  navigationButtonPressed({ buttonId }) {
+    if (buttonId === "btn_toggle_drawer2") {
+      !this.isSideDrawerVisible
+        ? (this.isSideDrawerVisible = true)
+        : (this.isSideDrawerVisible = false);
+      Navigation.mergeOptions(this.props.componentId, {
+        sideMenu: {
+          left: {
+            visible: this.isSideDrawerVisible
+          }
         }
+      });
     }
-    
+  }
 
-    placeAddedHandler=(placeName)=>{
-        this.props.actions.addPlace(placeName);
+  placeNameChangedHandler = val => {
+    this.setState({
+      placeName: val
+    });
+  };
+
+  placeAddedHandler = () => {
+    if(this.state.placeName.trim() !==''){
+        this.props.actions.addPlace(this.state.placeName);
     }
-    render(){
-        return(
-            <View>
-                <PlaceInput onPlaceAdded={this.placeAddedHandler}/>
-            </View>
-        );
-    }
+  };
+  render() {
+    return (
+      <ScrollView>
+        <View style={styles.container}>
+          <MainText>
+            <HeadingText>Share a Place With Us!</HeadingText>
+          </MainText>
+          <PickImage />
+          <PickLocation />
+          <PlaceInput
+            placeName={this.state.placeName}
+            onChangeText={this.placeNameChangedHandler}
+          />
+          <View style={styles.button}>
+            <Button
+              title="Share the Place!"
+              onPress={this.placeAddedHandler}
+            />
+          </View>
+        </View>
+      </ScrollView>
+    );
+  }
 }
 
-mapDispatchToProps=(dispatch)=>{
-    return {actions:bindActionCreators({addPlace},dispatch)}
-}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center"
+  },
+  placeholder: {
+    borderWidth: 1,
+    borderColor: "black",
+    backgroundColor: "#eee",
+    width: "80%",
+    height: 150,
+    flex: 1
+  },
+  button: {
+    margin: 8
+  },
+  previewImage: {
+    width: "100%",
+    height: "100%"
+  }
+});
+
+mapDispatchToProps = dispatch => {
+  return { actions: bindActionCreators({ addPlace }, dispatch) };
+};
 
 export default connect(null,mapDispatchToProps)(SharePlace);
--- src/screens/SideDrawer/SideDrawer.js
+++ src/screens/SideDrawer/SideDrawer.js
@@ -1,31 +1,57 @@
-import React,{Component} from 'react';
-import {View,Text,Dimensions,StyleSheet} from 'react-native';
-import {Navigation} from 'react-native-navigation';
-class SideDrawer extends Component{
-    constructor(props){
-        super(props);
-        Navigation.mergeOptions(this.props.componentId, {
-            sideMenu: {
-                left: {
-                    visible: false
-                }
-            }
-        }); 
-    }
-    render(){
-        return(
-            <View style={[styles.container,{width:Dimensions.get("window").width * 0.8}]}>
-                <Text>Side Drawer</Text>    
-            </View>
-        );
-    }
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  Dimensions,
+  StyleSheet,
+  TouchableOpacity
+} from "react-native";
+import { Navigation } from "react-native-navigation";
+import Icon from "../../../node_modules/react-native-vector-icons/Ionicons";
+class SideDrawer extends Component {
+  constructor(props) {
+    super(props);
+    Navigation.mergeOptions(this.props.componentId, {
+      sideMenu: {
+        left: {
+          visible: false
+        }
+      }
+    });
+  }
+  render() {
+    return (
+      <View
+        style={[
+          styles.container,
+          { width: Dimensions.get("window").width * 0.8 }
+        ]}
+      >
+        <TouchableOpacity>
+          <View style={styles.drawerItem}>
+            <Icon name="ios-log-out" size={30} color="#aaa" style={styles.drawerItemIcon}/>
+            <Text>Sign Out</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
-const styles=StyleSheet.create({
-    container:{
-        paddingTop:22,
-        backgroundColor:"white",
-        flex:1
-    }
-})
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+    backgroundColor: "white",
+    flex: 1
+  },
+  drawerItem:{
+      flexDirection:"row",
+      alignItems:"center",
+      padding:10,
+      backgroundColor:'#eee'
+  },
+  drawerItemIcon:{
+      marginRight:10
+  }
+});
 
 export default SideDrawer;
